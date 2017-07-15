1d14014ead305207cb4c9d6a200185057cb0c13e
Jess <jessrrobins@gmail.com>
2017-07-15 02:08:41 +0000

    changing colors around

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,9 @@ import './index.css'
 
 import registerServiceWorker from './registerServiceWorker'
 
-import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import {amber500, green500, green700, green100, grey900, grey600, grey400, white} from 'material-ui/styles/colors'
 
 import Routes from './routes'
 
@@ -16,8 +16,22 @@ import injectTapEventPlugin from 'react-tap-event-plugin'
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin()
 
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: green500,
+    primary2Color: green700,
+    primary3Color: green100,
+    accent1Color: amber500,
+    textColor: grey900,
+    secondaryTextColor: grey600,
+    alternateTextColor: white,
+    canvasColor: white,
+    borderColor: grey400
+  }
+})
+
 ReactDOM.render(
-  <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
+  <MuiThemeProvider muiTheme={getMuiTheme(muiTheme)}>
     <Routes />
   </MuiThemeProvider>,
   document.getElementById('root')
