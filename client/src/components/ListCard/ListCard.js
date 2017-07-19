104878ad9e3f48cd7271db5ebc5af776ad2b60e2
Jess <jessrrobins@gmail.com>
2017-07-19 23:53:00 +0000

    added in some of the masonry stuff

--- client/src/components/ListCard/ListCard.js
+++ client/src/components/ListCard/ListCard.js
@@ -3,20 +3,24 @@ import {Card, CardActions, CardTitle, CardText} from 'material-ui/Card'
 import FlatButton from 'material-ui/FlatButton'
 import LinearProgress from 'material-ui/LinearProgress'
 
+import './ListCard.css'
+
 class ListCard extends Component {
   render() {
     const { list } = this.props
 
     return (
-      <Card>
+      <Card className="list-card">
         <CardTitle title={list.fullName} subtitle={`${list.fractionComplete.denominator} items`} />
         <CardText>
           <LinearProgress mode="determinate" value={list.percentComplete} />
-          {
-           list.items.map( (item, index) => {
-             return `${item.text}${index === list.items.length - 1 ? '' : ', '}`
-           })
-          }
+        </CardText>
+        <CardText>
+        {
+          list.items.map( (item, index) => {
+            return `${item.text}${index === list.items.length - 1 ? '' : ', '}`
+          })
+        }
         </CardText>
         <CardActions>
           <FlatButton label="Go!" />
--- client/src/components/MasonryLayout/MasonryLayout.js
+++ client/src/components/MasonryLayout/MasonryLayout.js
@@ -3,15 +3,38 @@ import MasonryInfiniteScroller from 'react-masonry-infinite'
 
 import ListCard from '../ListCard/ListCard'
 
+import './MasonryLayout.css'
+
+const sizes = [{
+  columns: 1,
+  gutter: 20
+}, {
+  mq: '768px',
+  columns: 2,
+  gutter: 20
+}, {
+  mq: '1024px',
+  columns: 3,
+  gutter: 20
+}, {
+  mq: '1200px',
+  columns: 4,
+  gutter: 20
+}]
+
 class MasonryLayout extends Component {
   render() {
 
     return (
-      <div>
-      {
-        this.props.lists.map( (list, index) => <ListCard key={index} list={list} />)
-      }
-      </div>
+      <MasonryInfiniteScroller
+        className="masonry-layout"
+        sizes={sizes}
+        hasMore={true}
+        loadMore={() => console.log('loading more!')}>
+        {
+          this.props.lists.map( (list, index) => <ListCard key={index} list={list} />)
+        }
+      </MasonryInfiniteScroller>
     )
   }
 }
