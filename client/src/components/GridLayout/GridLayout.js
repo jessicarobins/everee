02cf9014548375d2f01639e6412aee65e272e114
Jess <jessrrobins@gmail.com>
2017-10-09 23:56:35 +0000

    that's better

--- client/src/components/GridLayout/GridLayout.js
+++ client/src/components/GridLayout/GridLayout.js
@@ -26,34 +26,36 @@ class GridLayout extends Component {
 
     return (
       <div className='container' style={styles.container}>
-        <InfiniteScroll
-          pageStart={1}
-          loadMore={this.props.fetchLists}
-          hasMore={!this.props.isOutOfPages && !this.props.isLoading}
-          initialLoad={false}
-          loader={<div className="loader">Loading ...</div>}>
-          <GridList
-            cols={4}
-            cellHeight={200}
-            padding={1}>
+        { !this.props.spinner &&
+          <InfiniteScroll
+            pageStart={1}
+            loadMore={this.props.fetchLists}
+            hasMore={!this.props.isOutOfPages && !this.props.isLoading}
+            initialLoad={false}
+            loader={<div className="loader">Loading ...</div>}>
+            <GridList
+              cols={4}
+              cellHeight={200}
+              padding={1}>
 
-              {
-                this.props.lists.map((list, index) => {
-                  const isFeatured = index % 9 === 0
-                  return (
-                    <ListTile
-                      pushState={this.props.pushState}
-                      key={index}
-                      list={list}
-                      cols={isFeatured ? 2 : 1}
-                      isFeatured={isFeatured}
-                      rows={1} />
-                    )
-                })
-              }
+                {
+                  this.props.lists.map((list, index) => {
+                    const isFeatured = index % 9 === 0
+                    return (
+                      <ListTile
+                        pushState={this.props.pushState}
+                        key={index}
+                        list={list}
+                        cols={isFeatured ? 2 : 1}
+                        isFeatured={isFeatured}
+                        rows={1} />
+                      )
+                  })
+                }
 
-          </GridList>
-        </InfiniteScroll>
+            </GridList>
+          </InfiniteScroll>
+        }
       </div>
     )
   }
