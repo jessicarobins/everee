3499216c7b464b4751d9fa8b03b5ce96fa85b280
Jess <jessrrobins@gmail.com>
2017-10-11 00:37:51 +0000

    fix bug where you could edit your own lists after logout

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -2,12 +2,11 @@ import axios from 'axios'
 
 import { isAuthenticated } from '../routes'
 
-const headers = {
-  'Accept': 'application/json',
-  'Content-Type': 'application/json'
-}
-
 export default function callApi(endpoint, {method = 'get', data, params} = {}) {
+  const headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  }
 
   const token = isAuthenticated() ? localStorage.getItem('id_token') : null
 
