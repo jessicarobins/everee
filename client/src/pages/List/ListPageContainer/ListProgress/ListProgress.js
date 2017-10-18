3356af9a1f91a2f995735ede2bba7efe049c5c73
Jess <jessrrobins@gmail.com>
2017-10-18 00:44:20 +0000

    stephen knows what the progress bar is now

--- client/src/pages/List/ListPageContainer/ListProgress/ListProgress.js
+++ client/src/pages/List/ListPageContainer/ListProgress/ListProgress.js
@@ -1,23 +1,26 @@
 import React, { Component } from 'react'
 
-import Paper from 'material-ui/Paper'
 import Chip from 'material-ui/Chip'
 import LinearProgress from 'material-ui/LinearProgress'
+import {Card, CardHeader, CardText} from 'material-ui/Card'
 
 class ListProgress extends Component {
 
   renderProgress = () => {
     const { list } = this.props
     return (
-      <Paper className="list-progress list-detail">
-        <LinearProgress
-          mode="determinate"
-          value={list.percentComplete} />
-        <div className="list-chips">
-          <Chip>{list.percentComplete} %</Chip>
-          <Chip>{list.fractionComplete.total} items</Chip>
-        </div>
-      </Paper>
+      <Card className="list-progress">
+        <CardHeader subtitle="Progress"/>
+        <CardText>
+          <LinearProgress
+            mode="determinate"
+            value={list.percentComplete || 1} />
+          <div className="list-chips">
+            <Chip>{list.percentComplete}% complete</Chip>
+            <Chip>{list.fractionComplete.total} items</Chip>
+          </div>
+        </CardText>
+      </Card>
     )
   }
 
